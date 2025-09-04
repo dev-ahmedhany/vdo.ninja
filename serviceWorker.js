5b8f43098371785cbf2f1c21f9c128d629f11c75
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2025-09-04 07:14:31 +0300

    pwa

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,51 @@
- 
+const CACHE_NAME = 'vdo-ninja-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.css',
+  '/main.js',
+  '/lib.js',
+  '/webrtc.js',
+  '/manifest.json'
+  // Add more files as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // If fetch succeeds, clone the response and cache it
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseClone));
+        }
+        return response;
+      })
+      .catch(() => {
+        // If fetch fails, try to get from cache
+        return caches.match(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
